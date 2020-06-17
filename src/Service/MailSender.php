a7a8025455def6a96b862664ec37b33f9e6c3bd0
alex-bobrik <sasha.bobrik@inbox.ru>
2020-06-17 17:09:03 +0300

    Update email sending via Mailtrap

--- src/Service/MailSender.php
+++ src/Service/MailSender.php
@@ -6,25 +6,31 @@ namespace App\Service;
 
 use Symfony\Component\Mailer\Bridge\Mailgun\Transport\MailgunSmtpTransport;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Mime\RawMessage;
 
 class MailSender
 {
     private $mailer;
 
-    public function __construct(\Swift_Mailer $mailer)
+    public function __construct(MailerInterface $mailer)
     {
         $this->mailer = $mailer;
     }
 
     public function send(string $title, string $to, $body)
     {
-        $message = (new \Swift_Message($title))
-            ->setFrom('example@example.com')
-            ->setTo($to)
-            ->setBody($body);
+        $email = (new Email())
+            ->from('quiz.mailer@quizzes.com')
+            ->to($to)
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject($title)
+            ->html($body);
 
-        $this->mailer->send($message);
+        $this->mailer->send($email);
 
     }
 }
